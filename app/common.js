f84002bc2fee03402c3007db825b675515e73a24
fhb <fhb@example.com>
2017-11-16 01:32:16 +0800

    优化登录功能

--- app/common.js
+++ app/common.js
@@ -1,4 +1,5 @@
-var SERVER_PRO_URL = "http://116.62.187.97:58080";
+//var SERVER_PRO_URL = "http://116.62.187.97:58080";
+var SERVER_PRO_URL = "http://127.0.0.1:58080";
 var SERVER_MEM_IP = "127.0.0.1";
 var proData = new Object();
 
